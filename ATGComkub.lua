2ed5e27f4324c2c5f287e8bacf356f1a92da11a2
ATGFAIL <167156538+ATGFAIL@users.noreply.github.com>
2025-10-05 01:56:53 +0700

    Update ATGComkub.lua

--- ATGComkub.lua
+++ ATGComkub.lua
@@ -3,7 +3,6 @@
 -- -----------------------
 local allowedPlaces = {
     [8069117419] = "demon", -- ตัวอย่างแมพ 1
-    [126509999114328] = "99afk",  -- ตัวอย่างแมพ 2
     [127742093697776] = "Plants-Vs-Brainrots",
     [96114180925459] = "Lasso-Animals"
 }
@@ -32,8 +31,6 @@ end
 -- เลือกโหลดตามแมพ
 if placeType == "demon" then
     loadExtraScript("https://raw.githubusercontent.com/ATGFAIL/ATGHub/main/demon.lua")
-elseif placeType == "99afk" then
-    loadExtraScript("https://raw.githubusercontent.com/ATGFAIL/ATGHub/main/99afk.lua")
 elseif placeType == "Plants-Vs-Brainrots" then
     loadExtraScript("https://raw.githubusercontent.com/ATGFAIL/ATGHub/main/Plants-Vs-Brainrots.lua")
 elseif placeType == "Lasso-Animals" then
