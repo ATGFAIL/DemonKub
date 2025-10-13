e8bdf771f2a0517f7f2ca4084373547722d52923
ATGFAIL <167156538+ATGFAIL@users.noreply.github.com>
2025-10-13 09:58:21 +0700

    Update ATGComkub.lua

--- ATGComkub.lua
+++ ATGComkub.lua
@@ -15,6 +15,7 @@ local allowedPlaces = {
     [136431686349723]     = { name = "DARK-DECEPTION-HUNTED", url = "https://api.luarmor.net/files/v3/loaders/4b264ea152dbb41b1916494d60607163.lua"},
     [142823291]           = { name = "Murder-Mystery-2",     url = "https://api.luarmor.net/files/v3/loaders/d48b61ec237a114790c3a9346aa4bedf.lua"},
     [126884695634066]     = { name = "Grow-a-Garden",        url = "https://api.luarmor.net/files/v3/loaders/30c274d8989e8c01a8c8fa3511756d0b.lua"},
+    [122826953758426]     = { name = "Raise-Animals",       url = "https://raw.githubusercontent.com/ATGFAIL/ATGHub/main/Raise-Animals.lua"},
 }
 
 -- -----------------------
